d988dd00915f53f76e271ba2728ba98516dd52eb
zcienka <zuzannacienka@gmail.com>
2022-12-16 04:00:08 +0100

    Fix wagonwpociagu bug

--- sql-configuration/wagonwpociagu.sql
+++ sql-configuration/wagonwpociagu.sql
@@ -1,18 +1,20 @@
 create table wagonwpociagu
 (
-    numerPociagu integer,
+    numerwagonu integer,
     idWagonu     integer,
     idpociagu    integer
 );
 
-insert into wagonWPociagu(numerPociagu, idWagonu, idpociagu)
+insert into wagonWPociagu(numerwagonu, idWagonu, idpociagu)
 values (1, 1, 1);
 
-insert into wagonWPociagu(numerPociagu, idWagonu, idpociagu)
+insert into wagonWPociagu(numerwagonu, idWagonu, idpociagu)
 values (2, 2, 2);
 
-insert into wagonWPociagu(numerPociagu, idWagonu, idpociagu)
+insert into wagonWPociagu(numerwagonu, idWagonu, idpociagu)
 values (3, 3, 3);
 
-insert into wagonWPociagu(numerPociagu, idWagonu, idpociagu)
+insert into wagonWPociagu(numerwagonu, idWagonu, idpociagu)
 values (4, 4, 4);
+
+
